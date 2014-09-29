b5b08b39dced22e12c6445498ac42e41ae2c2401
muneeb <muneeb.khan@it.uu.se>
2014-09-29 14:46:20 +0200

    repo for more hackish but very useful scripts that take long to write but are easier to copy/paste from when writing similar hackish scripts

---
+++ insert_pref.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+
+from optparse import OptionParser
+import sys
+import subprocess
+import re
+
+class Conf:
+    def __init__(self):
+        parser = OptionParser("usage: %prog [OPTIONS...] INFILE")
+
+        parser.add_option("-i",
+                          type="str", default=None,
+                          dest="src_file",
+                          help="Specify assembler (.s) file")
+
+        parser.add_option("-o",
+                          type="str", default=None,
+                          dest="outfile",
+                          help="Specify assembler (.s) output file with embedded prefetch instructions")
+
+        parser.add_option("-f",
+                          type="str", default=None,
+                          dest="infile",
+                          help="File containing prefetch decisions")
+                          
+        parser.add_option("-e",
+                          type="str", default=None,
+                          dest="exec_file",
+                          help="Executable file to inspect")
+
+        (opts, args) = parser.parse_args()
+
+        if not opts.infile or not opts.outfile or not opts.src_file \
+                or not opts.exec_file:
+            print >> sys.stderr, "Invalid argument, parameter missing!"
+            sys.exit(1)
+
+        self.src_file = opts.src_file
+        self.outfile = opts.outfile
+        self.infile = opts.infile
+        self.exec_file = opts.exec_file
+        self.re_memop = re.compile("(?:0[xX][0-9a-fA-F]+|0[bB][01]*|\d+|[a-zA-Z_.$][0-9a-zA-Z_.$]*)?\([^)]*\)")
+        self.re_offset = re.compile("[\s]+0[xX][0-9a-fA-F]+|[\s]+\d+")
+        self.re_regs = re.compile("\([^)]*\)")
+        self.ind_regs = re.compile("\%r[0-9a-zA-z]+")
+
+def main():
+
+    conf = Conf()
+
+    src_file = open(conf.src_file, "r")
+    infile = open(conf.infile, "r")
+    outfile = open(conf.outfile, "w")
+
+    pref_dec_dict = {}
+
+    for line in infile:
+
+        fline = line.rstrip()
+        fline_tok = fline.split(":")
+
+        insert_at_PC = fline_tok[0]
+        pref_type = fline_tok[1]
+        pref_dist = int(fline_tok[2])
+
+        src_FILE_LINE = subprocess.Popen(["/home/muneeb/llvm-3.3/Release+Asserts/bin/llvm-dwarfdump", "-address="+insert_at_PC, conf.exec_file], stdout=subprocess.PIPE).communicate()[0]
+
+        src_file_from_dbg = src_FILE_LINE.split(":")[0]
+        src_line = int(src_FILE_LINE.split(":")[1])
+        print src_file_from_dbg, src_line
+
+        pref_dec_dict[src_line] = [pref_type, pref_dist]
+
+    lineno = 1
+    for line in src_file:
+        
+        if lineno in pref_dec_dict:
+            memop = conf.re_memop.findall(line)
+            offset = conf.re_offset.findall(line)
+            regs = conf.re_regs.findall(memop[0])
+        
+            [pref_type, sd] = pref_dec_dict[lineno]
+        
+            off = long(offset[0]) + int(sd)
+            pref_reg_off = str(off)+regs[0]
+            
+            if pref_type == 'nta':
+                outfile.write("\n\tprefetchnta %s\n"%(pref_reg_off))
+            else:
+                outfile.write("\n\tprefetcht0 %s\n"%(pref_reg_off))
+
+        outfile.write(line)
+
+        lineno += 1
+
+    src_file.close()
+    infile.close()
+    outfile.close()
+
+if __name__ == "__main__":
+    main()
