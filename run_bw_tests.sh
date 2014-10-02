ec08cbe8da169072d9de1be8ff5fd1b46643a364
Muneeb Khan <muneeb.khan@it.uu.se>
2014-10-02 19:24:01 +0200

    fixed offcore response event (LLC_MISS_LOCAL_DRAM and not LLC_MISS_LOCAL) and ported event to Sandybridge instead of Haswell

--- run_bw_tests.sh
+++ run_bw_tests.sh
@@ -3,35 +3,38 @@
 SPEC_HOME=/home/muneeb/spec2006_static
 SCRIPT_HOME=/home/muneeb/git/scripts
 
-${SCRIPT_HOME}/hwpf.sh off
+for HWPF_SETTINGS in on off
 
-for BENCH in mcf lbm soplex
-do
+    ${SCRIPT_HOME}/hwpf.sh ${HWPF_SETTINGS}
 
-    BENCH_DIR=${SPEC_HOME}/*${BENCH}*/src.clean
-
-    cd ${BENCH_DIR}
-
-    INFO_STR=""
-
-    OUTFILE=/home/muneeb/protean/${BENCH}_perins_bw.csv
-
-    echo "#num nta, CYCLES, MEM_BW, L3_MISSES, L2_MISSES" > ${OUTFILE}
-
-    for NUM_NTA in $(seq 0 15)
+    for BENCH in mcf lbm soplex
     do
-        ARGS=$(grep PARAM Makefile | tr -d 'PARAMS=' )
 
-        LD_LIBRARY_PATH=/home/muneeb/llvm-3.3/Release+Asserts/lib:/home/muneeb/llvm-3.3/lib/Transforms/SBO/runtime/Scheduler/lib:/home/muneeb/llvm-3.3/lib/Transforms/SBO/runtime/Scheduler/src:/home/muneeb/llvm-3.3/lib/Transforms/SBO/runtime:/home/muneeb/perfmon2-libpfm4/lib/ SCHEDULER_PROFILE_FILE=.tmp PRT_ENABLED=0 /home/muneeb/papi_tools/papi_profiler -n 4 -e L2_RQSTS:DEMAND_DATA_RD_MISS -e OFFCORE_RESPONSE_0:ANY_REQUEST:LLC_MISS_LOCAL -e LONGEST_LAT_CACHE:MISS -e UNHALTED_CORE_CYCLES -o .tmp -t ./${BENCH}_protean_nta${NUM_NTA}.frmasm ${ARGS}
+        BENCH_DIR=${SPEC_HOME}/*${BENCH}*/src.clean
 
-        L3_MISSES=$(grep "OFFCORE_RESPONSE_0:ANY_REQUEST:LLC_MISS_LOCAL:" .tmp | awk '{print $2}')
-        L2_MISSES=$(grep "L2_RQSTS:DEMAND_DATA_RD_MISS:" .tmp | awk '{print $2}')
-        CYCLES=$(grep "UNHALTED_CORE_CYCLES:" .tmp | awk '{print $2}')
-        MEM_BW=$(echo "("${L3_MISSES}"*64/1024^3)/("${CYCLES}"/4000000000)" | bc -l)
+        cd ${BENCH_DIR}
 
-        INFO_STR=${NUM_NTA}" "${CYCLES}" "${MEM_BW}" "${L3_MISSES}" "${L2_MISSES}
+        INFO_STR=""
 
-        echo ${INFO_STR} >> ${OUTFILE}
+        OUTFILE=/home/muneeb/protean/${BENCH}_perins_bw_hwpf${HWPF_SETTINGS}.csv
+
+        echo "#num_nta, CYCLES, MEM_BW, L3_MISSES, L2_HITS" > ${OUTFILE}
+
+        for NUM_NTA in $(seq 0 15)
+        do
+            ARGS=$(grep PARAM Makefile | tr -d 'PARAMS=' )
+
+            LD_LIBRARY_PATH=/home/muneeb/llvm-3.3/Release+Asserts/lib:/home/muneeb/llvm-3.3/lib/Transforms/SBO/runtime/Scheduler/lib:/home/muneeb/llvm-3.3/lib/Transforms/SBO/runtime/Scheduler/src:/home/muneeb/llvm-3.3/lib/Transforms/SBO/runtime:/home/muneeb/perfmon2-libpfm4/lib/ SCHEDULER_PROFILE_FILE=.tmp PRT_ENABLED=0 /home/muneeb/papi_tools/papi_profiler -n 4 -e MEM_LOAD_UOPS_RETIRED:L2_HIT -e OFFCORE_RESPONSE_0:ANY_REQUEST:LLC_MISS_LOCAL_DRAM -e LLC_MISSES -e UNHALTED_CORE_CYCLES -o .tmp -t ./${BENCH}_protean_nta${NUM_NTA}.frmasm ${ARGS}
+
+            L3_MISSES=$(grep "OFFCORE_RESPONSE_0:ANY_REQUEST:LLC_MISS_LOCAL_DRAM:" .tmp | awk '{print $2}')
+            L2_HITS=$(grep "MEM_LOAD_UOPS_RETIRED:L2_HIT:" .tmp | awk '{print $2}')
+            CYCLES=$(grep "UNHALTED_CORE_CYCLES:" .tmp | awk '{print $2}')
+            MEM_BW=$(echo "("${L3_MISSES}"*64/1024^3)/("${CYCLES}"/3400000000)" | bc -l)
+
+            INFO_STR=${NUM_NTA}" "${CYCLES}" "${MEM_BW}" "${L3_MISSES}" "${L2_HITS}
+
+            echo ${INFO_STR} >> ${OUTFILE}
+        done
+
     done
-
 done
