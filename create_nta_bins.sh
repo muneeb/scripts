348a89a9387b5b0d40609218dec61fc2ed018121
Muneeb Khan <muneeb.khan@it.uu.se>
2014-10-06 17:32:36 +0200

    now extracting lines directly from binary image --  removed bugs related to macros(??) in assembler

--- create_nta_bins.sh
+++ create_nta_bins.sh
@@ -5,7 +5,7 @@ SPEC_HOME=/home/muneeb/spec2006_static
 
 INFILE=${1}
 
-for BENCH in lbm mcf soplex
+for BENCH in mcf #soplex #lbm
 do
 
     BENCH_DIR=${SPEC_HOME}/*${BENCH}*/src.clean
@@ -15,24 +15,35 @@ do
     ASM_SRC_FILE=${BENCH}_protean.s
     EXEC_FILE=${BENCH}_protean.frmasm
 
-    for LINES in $(seq 2 16)
+    for LINES in $(seq 3 16)
     do
 
         rm -f .tmp
 
-        OPT_EXE_BASE_NAME=${BENCH}_protean_nta$((${LINES}-1))
+        OPT_EXE_BASE_NAME=${BENCH}_protean_nta_$((${LINES}-1))_amd
         ASM_OUTFILE=${OPT_EXE_BASE_NAME}.s
 
-        for CURR_LINE in $(seq 2 ${LINES})
+        for CURR_LINE in $(seq 3 ${LINES})
         do
-            ADDR=$(sed ${CURR_LINE}'q;d' ${SCRIPTS_HOME}/${BENCH}_protean.perinsntabw | awk '{print $1}')
+            ADDR=$(sed ${CURR_LINE}'q;d' ${SCRIPTS_HOME}/${BENCH}_protean_amd.perinsntabw | awk '{print $1}')
             echo ${ADDR}':nta:0' >> .tmp
 
         done
 
         ${SCRIPTS_HOME}/insert_pref.py -i ${ASM_SRC_FILE} -o ${ASM_OUTFILE} -f .tmp -e ${EXEC_FILE}
 
-        /home/muneeb/llvm-3.3/Release+Asserts/bin/clang -O3 -sbo ${ASM_OUTFILE} -o ${OPT_EXE_BASE_NAME}.sbo.nobc -L/home/muneeb/llvm-3.3/lib/Transforms/SBO/runtime -lSBO_runtime -ldl -lrt  -Wl,--export-dynamic
+        BENCH_LDFLAGS=$(grep "BENCH_LDFLAGS=" Makefile.clang | tr -d 'BENCH_LDFLAGS=')
+
+        BENCHLANG=$(grep "BENCHLANG=" Makefile.clang | tr -d 'BENCHLANG=')
+
+        if [[ ${BENCHLANG} == "C" ]]
+        then
+            BENCHLANG=/home/muneeb/llvm-3.3/Release+Asserts/bin/clang
+        else
+            BENCHLANG=/home/muneeb/llvm-3.3/Release+Asserts/bin/clang++
+        fi
+
+        ${BENCHLANG} -O3 -sbo ${ASM_OUTFILE} -o ${OPT_EXE_BASE_NAME}.sbo.nobc -L/home/muneeb/llvm-3.3/lib/Transforms/SBO/runtime -lSBO_runtime -ldl -lrt ${BENCH_LDFLAGS} -Wl,--export-dynamic
 
         objcopy --add-section .sbo_ir=${BENCH}_protean.sbo.ll ${OPT_EXE_BASE_NAME}.sbo.nobc ${OPT_EXE_BASE_NAME}.frmasm
 
